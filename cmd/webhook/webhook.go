90c23300a30ff47b459313efad0c382d09fce563
IssacRunmin <ormmroorm@gmail.com>
2021-08-09 11:04:52 +0800

    Update webhook.go
    add port command line option

--- cmd/webhook/webhook.go
+++ cmd/webhook/webhook.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"net/http"
-
+	"os"
 	"github.com/gin-gonic/gin"
 	model "github.com/IssacRunmin/alertmanaer-cqhttp-webhook/model"
 	"github.com/IssacRunmin/alertmanaer-cqhttp-webhook/notifier"
@@ -12,11 +12,13 @@ import (
 var (
 	h            bool
 	defaultRobot string
+	listenPort string
 )
 
 func init() {
 	flag.BoolVar(&h, "h", false, "help")
 	flag.StringVar(&defaultRobot, "defaultRobot", "", "global cqhttp robot webhook, you can overwrite by alert rule with annotations cqRobot")
+	flag.StringVar(&listenPort, "port", "5601", "global webhook listening port, default 5601")
 }
 
 func main() {
@@ -27,7 +29,10 @@ func main() {
 		flag.Usage()
 		return
 	}
-
+	err := os.Setenv("PORT", listenPort)
+	if err != nil {
+		return 
+	}
 	router := gin.Default()
 	router.POST("/webhook", func(c *gin.Context) {
 		var notification model.Notification
